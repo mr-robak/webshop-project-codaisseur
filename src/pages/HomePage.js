b6cb6536b6134c1fdd549200fe645384a15ae9fc
contexD <maxconte232@gmail.com>
2020-05-19 15:27:30 +0200

    selectors tested

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  selectAllProducts,
+  selectProductById,
+} from "../store/products/selectors";
+import { selectItemsInCart } from "../store/cart/selectors";
+
+export default function HomePage() {
+  return <div>test</div>;
+}
