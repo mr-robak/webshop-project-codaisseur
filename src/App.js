7be73add6185c2dc63f75b073446eaf1c3f164c7
Marcin Robak <mr.robak@gmail.com>
2020-05-19 16:23:36 +0200

    add NavBar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <div>
+      <NavBar />
       <Switch>
         {/* more pages to be added here later */}
         <Route exact path="/" component={HomePage} />
