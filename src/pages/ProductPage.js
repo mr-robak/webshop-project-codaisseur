c033d4a44bd4fe1b0640df3e0269d8d2164926a2
contexD <maxconte232@gmail.com>
2020-05-20 14:26:47 +0200

    added card for product

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,11 +1,39 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectProductById } from "../store/products/selectors";
+import { formatPrice } from "../helper/helperFunctions";
 
 export default function ProductPage() {
-  const prodId = useParams().id;
+  //get prod Id from route
+  const prodId = parseInt(useParams().id);
+  //get product details
+  const product = useSelector(selectProductById(prodId));
+  console.log("product", product);
+
   return (
-    <div>
-      <h1>Test Product Page {prodId}</h1>
+    <div className="container my-3">
+      <div className="card mb-3 w-50">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img
+              src={product.imageUrl}
+              className="card-img"
+              alt="image of product"
+            />
+            <h3 className="my-3">{formatPrice(product.price)}</h3>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{product.name}</h5>
+              <p className="card-text">{product.description}</p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
