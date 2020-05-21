8f2834375519d5cd611011dd98b83c260283b0f6
contexD <maxconte232@gmail.com>
2020-05-21 21:33:12 +0200

    fixed placement of buttons on product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,8 +13,8 @@ export default function Product(props) {
       <div className="row no-gutters">
         <div className="col-md-4">
           <img src={props.url} className="card-img" alt=" product" />
-          <h3 className="my-3">{formatPrice(props.price)}</h3>
-          <div className="row w-90">
+          <h3 className="my-3 mx-3">{formatPrice(props.price)}</h3>
+          <div className="row mx-2 my-2">
             <RemoveFromCartButton id={props.id} />
             <h5>{item === undefined ? 0 : item.amount} in cart</h5>
             <AddToCartButton id={props.id} />
@@ -24,9 +24,6 @@ export default function Product(props) {
           <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             <p className="card-text">{props.description}</p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
           </div>
         </div>
       </div>
